2b5bd318ba0f83b2d2c1251820ce0aa06f51bca6
chalermsak-s <kaidohjung@gmail.com>
2025-02-16 00:03:03 +0700

    Task 11.2
    https://qiwwqmigxpucshdwfzup.supabase.co/storage/v1/object/public/images/books/b57528ef0685bc0a09fe8e286bf2efa9.jpeg

--- src/server.ts
+++ src/server.ts
@@ -39,6 +39,21 @@ app.post('/upload', upload.single('file'), async (req: any, res: any) => {
   }
 })
 
+app.post('/book/upload', upload.single('file'), async (req: any, res: any) => {
+  try {
+    const file = req.file
+    if (!file) {
+      return res.status(400).send('No file uploaded.')
+    }
+    const bucket = 'images'
+    const filePath = `books`
+    const ouputUrl = await uploadFile(bucket, filePath, file)
+    res.status(200).send(ouputUrl)
+  } catch (error) {
+    res.status(500).send('Error uploading file.')
+  }
+})
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Events API')
 })
--- src/services/bookServices.ts
+++ src/services/bookServices.ts
@@ -1,20 +1,47 @@
 import { Book } from '../models/book'
 import * as repo from '../repository/bookRepositoryDb'
+import s3Client from '../awsConfig'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
+import { randomBytes } from 'crypto'
 
 export { Book }
 
-export function getBookByTitle(title: string): Promise<Book[]> {
-  return repo.getBookByTitle(title)
+export const getBookByTitle = (title: string): Promise<Book[]> =>
+  repo.getBookByTitle(title)
+export const getAllBooks = (): Promise<Book[]> => repo.getAllBooks()
+export const getBookById = (id: number): Promise<Book | undefined> =>
+  repo.getBookById(id)
+export const addBook = (newBook: Book): Promise<Book> => repo.addBook(newBook)
+
+const generateSaltedFilename = (originalName: string): string => {
+  const salt = randomBytes(16).toString('hex')
+  const extension = originalName.split('.').pop() || ''
+  return `${salt}.${extension}`
 }
 
-export function getAllBooks(): Promise<Book[]> {
-  return repo.getAllBooks()
-}
+export const uploadFile = async (
+  bucket: string,
+  filePath: string,
+  file: Express.Multer.File
+): Promise<string> => {
+  const saltedFilename = generateSaltedFilename(file.originalname)
+  const saltedFilePath = `${filePath}/${saltedFilename}`
 
-export function getBookById(id: number): Promise<Book | undefined> {
-  return repo.getBookById(id)
-}
+  try {
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: bucket,
+        Key: saltedFilePath,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      })
+    )
 
-export function addBook(newBook: Book): Promise<Book> {
-  return repo.addBook(newBook)
+    const publicUrl = `https://qiwwqmigxpucshdwfzup.supabase.co/storage/v1/object/public/images/${saltedFilePath}`
+    console.log('File uploaded successfully:', publicUrl)
+    return publicUrl
+  } catch (error) {
+    console.error('Error uploading file:', error)
+    throw error
+  }
 }
