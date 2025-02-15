3463ae9297dbcb0fd5010d0bc5258d5742f51d57
chalermsak-s <kaidohjung@gmail.com>
2025-02-15 23:44:05 +0700

    Task 10.16 - 10.17
    Upload the file and get the URL:
    
    **https://qiwwqmigxpucshdwfzup.supabase.co/storage/v1/object/public/images/uploads/true.jpeg**
    
    However, I skipped setting the key, making it public by default.

--- src/server.ts
+++ src/server.ts
@@ -30,13 +30,10 @@ app.post('/upload', upload.single('file'), async (req: any, res: any) => {
     if (!file) {
       return res.status(400).send('No file uploaded.')
     }
-
     const bucket = 'images'
     const filePath = `uploads/${file.originalname}`
-
-    await uploadFile(bucket, filePath, file)
-
-    res.status(200).send('File uploaded successfully.')
+    const ouputUrl = await uploadFile(bucket, filePath, file)
+    res.status(200).send(ouputUrl)
   } catch (error) {
     res.status(500).send('Error uploading file.')
   }
--- src/services/uploadFileService.ts
+++ src/services/uploadFileService.ts
@@ -5,19 +5,21 @@ export async function uploadFile(
   bucket: string,
   filePath: string,
   file: Express.Multer.File
-): Promise<void> {
+): Promise<string> {
   const params = {
     Bucket: bucket,
     Key: filePath,
     Body: file.buffer,
     ContentType: file.mimetype,
   }
-
   try {
     const data = await s3Client.send(new PutObjectCommand(params))
     console.log('File uploaded successfully:', data)
+    const publicUrl = `https://qiwwqmigxpucshdwfzup.supabase.co/storage/v1/object/public/images/${filePath}`
+    console.log('File uploaded successfully:', publicUrl)
+    return publicUrl
   } catch (error) {
-    console.error('Error uploading file:', error)
+    console.log('Error uploading file:', error)
     throw error
   }
 }
